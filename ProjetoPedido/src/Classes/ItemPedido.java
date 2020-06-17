23ca758911f4cf2aaff700480126ad4d90854f11
Matheus <rossatto.gomes30@gmail.com>
2020-06-16 22:35:18 -0300

    Classes sem implementação

--- ProjetoPedido/src/Classes/ItemPedido.java
+++ ProjetoPedido/src/Classes/ItemPedido.java
@@ -1,18 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Classes;
 
-/**
- *
- * @author Dark
- */
 public class ItemPedido {
-    
+
     //Atributos da classe ItemPedido
-    protected long id;
-    protected double valorItem;
-    protected int quantidade;
+    private long id;
+    private double valorItem;
+    private int quantidade;
+    private long idPedido;
+    private long idItem;
+    private Pedido pedido;
+    private Item item;
+
+    public ItemPedido(long id, double valorItem, int quantidade, long idPedido, long idItem) {
+        this.id = id;
+        this.valorItem = valorItem;
+        this.quantidade = quantidade;
+        this.idPedido = idPedido;
+        this.idItem = idItem;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getValorItem() {
+        return valorItem;
+    }
+
+    public void setValorItem(double valorItem) {
+        this.valorItem = valorItem;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public long getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(long idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public long getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(long idItem) {
+        this.idItem = idItem;
+    }
 }
