315eae2ea01186efcc6af10341fe76b71c9b9f30
Matheus <rossatto.gomes30@gmail.com>
2020-06-16 23:14:38 -0300

    Repositories

---
+++ ProjetoPedido/src/Repositories/ItemPedidoRepository.java
@@ -0,0 +1,34 @@
+package Repositories;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ItemPedidoRepository {
+
+    private File file;
+    private BufferedReader buferedReader;
+    private BufferedWriter buferedWriter;
+
+    public ItemPedidoRepository() throws IOException {
+        this.file = new File("ItemPedidoRepository.txt");
+        this.buferedReader = new BufferedReader(new FileReader(this.file));
+        this.buferedWriter = new BufferedWriter(new FileWriter(this.file, true));
+    }
+
+    public BufferedReader getBuferedReader() {
+        return buferedReader;
+    }
+
+    public BufferedWriter getBuferedWriter() {
+        return buferedWriter;
+    }
+
+    public void Close() throws IOException {
+        this.buferedReader.close();
+        this.buferedWriter.close();
+    }
+}
